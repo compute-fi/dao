15d84573935d4056de9a18277389a177dd5b8735
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2023-11-28 21:46:52 +0530

    feat: complete unit test for Governor contract
    Signed-off-by: Aditya Kumar Singh <sadityakumar9211@gmail.com>

--- test/MyGovernorTest.t.sol
+++ test/MyGovernorTest.t.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
+import "forge-std/console.sol";
 import {MyGovernor} from "../src/MyGovernor.sol";
 import {Box} from "../src/Box.sol";
 import {Timelock} from "../src/Timelock.sol";
@@ -18,9 +19,16 @@ contract MyGovernorTest is Test {
     uint256 public constant INITIAL_SUPPLY = 100 ether;
 
     uint256 public constant MIN_DELAY = 3600;
+    uint256 public constant VOTING_DELAY = 1; // how many blocks the proposal is active
+    uint256 public constant VOTING_PERIOD = 50400; // how many blocks the proposal is active
+
     address[] proposers;
     address[] executors;
     address[] admins = [USER];
+
+    uint256[] values = [0];
+    bytes[] calldatas;
+    address[] targets;
 
     function setUp() public {
         govToken = new GovToken();
@@ -48,5 +56,57 @@ contract MyGovernorTest is Test {
         box.store(1);
     }
 
-    
+    function testGovernanceUpdateBox() public {
+        uint256 valueToStore = 111;
+        string memory description = "store 111 in Box";
+        bytes memory encodedFunctionCall = abi.encodeWithSignature(
+            "store(uint256)",
+            valueToStore
+        );
+        calldatas.push(encodedFunctionCall);
+        targets.push(address(box));
+
+        // 1. Propose to the DAO
+        uint256 proposalId = governor.propose(
+            targets,
+            values,
+            calldatas,
+            description
+        );
+
+        // View the state
+        console.log("Proposal State: ", uint256(governor.state(proposalId)));
+
+        vm.warp(block.timestamp + VOTING_DELAY + 1);
+        vm.roll(block.number + VOTING_DELAY + 1);
+
+        console.log("Proposal State: ", uint256(governor.state(proposalId)));
+
+        // 2. Vote
+        string memory reason = "cuz blue frog is cool";
+        uint8 voteWay = 1; // voting yes
+
+        vm.prank(USER);
+        governor.castVoteWithReason(proposalId, voteWay, reason);
+        vm.stopPrank();
+
+        vm.warp(block.timestamp + VOTING_PERIOD + 1);
+        vm.roll(block.number + VOTING_PERIOD + 1);
+
+        // 3. Queue
+        bytes32 descriptionHash = keccak256(abi.encodePacked(description));
+        governor.queue(targets, values, calldatas, descriptionHash);
+
+        vm.warp(block.timestamp + MIN_DELAY + 1);
+        vm.roll(block.number + MIN_DELAY + 1);
+
+        // 4. Execute
+        governor.execute(targets, values, calldatas, descriptionHash);
+
+        // 5. Check if the value is updated
+        uint256 storedValue = box.getNumber();
+        assertEq(storedValue, valueToStore);
+        console.log("Stored Values changed to ", storedValue);
+        console.log("Proposal State", uint256(governor.state(proposalId)));
+    }
 }
